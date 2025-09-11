7b86a0ca68dc57e50dbef51b326a172929802d2a
Kindual <110603179+Kindual@users.noreply.github.com>
2025-09-11 17:40:02 +0300

    init
    створення збірки

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				brand: '#1e40af',
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config;
