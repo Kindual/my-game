5e0bdb51b2b30ec74af31819fd0f04b6060c691c
Kindual <110603179+Kindual@users.noreply.github.com>
2025-09-15 16:06:43 +0300

    add CharacterPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Suspense } from 'react';
-import HomePage from './pages/home/home';
+import HomePage from './pages/home/homePage';
 import Loader from './comonents/loader';
 import BattlePage from './pages/battlepage/battlePage';
+import CharacterPage from './pages/character/characterPage';
 
 function App() {
 	return (
@@ -22,6 +23,14 @@ function App() {
 					element={
 						<Suspense fallback={<Loader />}>
 							<BattlePage />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/character"
+					element={
+						<Suspense fallback={<Loader />}>
+							<CharacterPage />
 						</Suspense>
 					}
 				/>
---
+++ src/pages/character/characterPage.tsx
@@ -0,0 +1,11 @@
+function CharacterPage() {
+	return (
+		<div className="battle-page">
+			<>
+				<div>"Тут буде персонаж"</div>
+			</>
+		</div>
+	);
+}
+
+export default CharacterPage;
