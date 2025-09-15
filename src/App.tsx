b7a4d8e71384dfae05e5f6d7ceca02b4e96cf61e
Kindual <110603179+Kindual@users.noreply.github.com>
2025-09-15 15:54:39 +0300

    add BattlePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-// import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Suspense } from 'react';
 import HomePage from './pages/home/home';
 import Loader from './comonents/loader';
+import BattlePage from './pages/battlepage/battlePage';
 
 function App() {
-	// const [count, setCount] = useState(0)
-
 	return (
 		<>
 			<Routes>
 				<Route
 					path="/"
 					element={
-						<Suspense fallback={<Loader/>}>
+						<Suspense fallback={<Loader />}>
 							<HomePage />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/game"
+					element={
+						<Suspense fallback={<Loader />}>
+							<BattlePage />
 						</Suspense>
 					}
 				/>
